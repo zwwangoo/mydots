4fa47749a21e8dea01acca287f1e16318fab2182
wangzhiwen <wangzhiwen@mchz.com.cn>
2024-04-25 00:15:26 +0800

    feat: nvim自动安装适配centos7

--- nvim/install.sh
+++ nvim/install.sh
@@ -13,11 +13,12 @@ install_nodejs(){
     fi
 
     if [ ! -x "$(command -v node)" ]; then
-        install_version=`curl -H 'Cache-Control: no-cache'  "https://api.github.com/repos/nodejs/node/releases/latest" | grep 'tag_name' | cut -d\" -f4`
+        # install_version=`curl -H 'Cache-Control: no-cache'  "https://api.github.com/repos/nodejs/node/releases/latest" | grep 'tag_name' | cut -d\" -f4`
+        install_version="v16.20.2"
         base_name="node-$install_version-$vdis"
         file_name=$base_name.tar.gz
         curl -L https://nodejs.org/dist/$install_version/$file_name -o $file_name
-        tar xzvf $file_name
+        tar -xzvf $file_name
         if [[ ! $? -eq 0 ]]; then 
             rm -rf $base_name*
             exit 1
@@ -46,34 +47,42 @@ install_neovim(){
         else
             exit 1
         fi
-
-        mkdir -p $HOME/.config/nvim
-
-        if [[ $SHELL == *"bash"* ]]; then
-            echo "alias vim='nvim'" >> ~/.bashrc
-            echo "alias vi='nvim'" >> ~/.bashrc
-            echo "Please run 'source ~/.bashrc' to apply the alias changes."
-        elif [[ $SHELL == *"zsh"* ]]; then
-            echo "alias vim='nvim'" >> ~/.zshrc
-            echo "alias vi='nvim'" >> ~/.zshrc
-            echo "Please run 'source ~/.zshrc' to apply the alias changes."
-        elif [[ $SHELL == *"fish"* ]]; then
-            echo "alias vim='nvim'" >> ~/.config/fish/config.fish
-            echo "alias vi='nvim'" >> ~/.config/fish/config.fish
-            echo "Please run 'source ~/.config/fish/config.fish' to apply the alias changes."
-        else
-            echo "Unsupported shell. Please add the aliases manually."
-        fi
-        export PATH="/usr/local/bin/:$PATH"
     fi
 
-    cp -R ./* $HOME/.config/nvim
-    nvim +Lazy +qall
+    mkdir -p $HOME/.config/nvim
 
+    if [[ $SHELL == *"bash"* ]]; then
+        echo "alias vim='nvim'" >> ~/.bashrc
+        echo "alias vi='nvim'" >> ~/.bashrc
+        echo "Please run 'source ~/.bashrc' to apply the alias changes."
+    elif [[ $SHELL == *"zsh"* ]]; then
+        echo "alias vim='nvim'" >> ~/.zshrc
+        echo "alias vi='nvim'" >> ~/.zshrc
+        echo "Please run 'source ~/.zshrc' to apply the alias changes."
+    elif [[ $SHELL == *"fish"* ]]; then
+        echo "alias vim='nvim'" >> ~/.config/fish/config.fish
+        echo "alias vi='nvim'" >> ~/.config/fish/config.fish
+        echo "Please run 'source ~/.config/fish/config.fish' to apply the alias changes."
+    else
+        echo "Unsupported shell. Please add the aliases manually."
+        exit 1
+    fi
+
+    # python
+    # TODO
+    # pip install pynvim neovim jedi-language-server
+
+    echo '[flake8]\nmax-line-length = 120' > $HOME/.config/flake8
+    cp -R ./* $HOME/.config/nvim
+    nvim +Lazy +UpdateRemotePlugins +qall
+
+    # Install coc
     cd $HOME/.local/share/nvim/lazy/coc.nvim
     npm install
-    echo '[flake8]\nmax-line-length = 120' > $HOME/.config/flake8
-
+    if [ -d "$HOME/.config/coc/extensions" ]; then
+        cd $HOME/.config/coc/extensions
+        npm install coc-jedi coc-json coc-git coc-docker coc-tabnine coc-imselect
+    fi
     nvim +CocInstall
 }
 
@@ -89,12 +98,19 @@ else
     fi
 
     if command -v apt-get &> /dev/null; then
-        apt-get install -y ripgrep git
+        apt-get install -y ripgrep
     elif command -v yum &> /dev/null; then
         # install rg
-        yum install -y yum-utils git
+        yum install -y yum-utils git gcc
         yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
         yum install -y ripgrep
+
+        # install git 2.36
+        if command -v git &> /dev/null; then
+            yum remove -y git
+        fi
+        yum install -y https://repo.ius.io/ius-release-el7.rpm https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+        yum install -y git236
     else
         echo "Neither apt-get, yum nor dnf found. Please install ripgrep and fd-find manually."
     fi
